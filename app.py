d2f9a7e87df654c3fdd1814b7d021fae64505fdf
Yossef-Dawoad <yossefdawoad15@gmail.com>
2023-06-13 19:58:08 +0300

    Improve code quality and performance with refactoring and dependency injection - Refactor code for better performance and readability. - Use dependency injection to create and reuse embedding model, llm and prompt template instances. - Use FastAPI's background tasks to process files and create embeddings asynchronously. - Add error handling and validation to endpoints for robustness and user-friendliness. - Add documentation and comments to explain code functionality and usage. - Add requirements.txt file for easy installation of dependencies.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Depends, BackgroundTasks
 from fastapi.responses import JSONResponse
 from pathlib import Path
 from langchain.document_loaders import UnstructuredFileLoader
@@ -15,77 +15,121 @@ app = FastAPI()
 
 # Initialize the embedding model and the llm
 embedding_model_name = "intfloat/e5-base-v2"
-embeddings = HuggingFaceEmbeddings(model_name=embedding_model_name)
+llm_repo_id = "tiiuae/falcon-7b-instruct"
 
-repo_id = "tiiuae/falcon-7b-instruct"
-llm = HuggingFaceHub(
-    repo_id=repo_id,
-    huggingfacehub_api_token=os.getenv("huggingfacehub_api_token"),
-    model_kwargs={"temperature": 0.6, "max_new_tokens": 1024},
-)
+# Define a dependency for creating an embedding model
+def get_embedding_model():
+    # embedding_model_name = 'intfloat/e5-base-v2'
+    embeddings = HuggingFaceEmbeddings(model_name=embedding_model_name)
+    return embeddings
 
-# Define a template for the prompt
-template = """
-You are a helpfull assistant. you gives helpful, summarized, and polite answers to the user's questions given the below context.
+# Define a dependency for creating an llm
+def get_llm():
+    # repo_id = "tiiuae/falcon-7b-instruct"
+    llm = HuggingFaceHub(
+        repo_id=llm_repo_id,
+        huggingfacehub_api_token='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',
+        model_kwargs={"temperature":0.6, "max_new_tokens":1024}
+    )
+    return llm
 
-{context}
+# Define a dependency for creating a prompt template
+def get_prompt_template():
+    template = """
+    You are a helpfull assistant. you gives helpful, summarized, and polite answers to the user's questions given the below context.
 
-question: {question}
-"""
-prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+    {context}
+
+    question: {question}
+    """
+    prompt = PromptTemplate(template=template, input_variables=['context', "question"])
+    return prompt
 
 # Define a global variable for the vector database
 vectordb = None
 
-
-# Define an endpoint for uploading text files and creating embeddings
-@app.post("/upload-files/")
-async def upload_files(files: list[UploadFile] = File(...)):
-    global vectordb  # use the global variable
+# Define a function for processing uploaded files and creating embeddings in the background
+def process_files(files: list[UploadFile], embeddings: HuggingFaceEmbeddings):
+    global vectordb # use the global variable
     # Create loaders for each file
     loaders = [UnstructuredFileLoader(file.file) for file in files]
     # Load the documents from each loader
     documents = [loader.load()[0] for loader in loaders]
     # Split the documents into text parts using RecursiveCharacterTextSplitter
     docs = RecursiveCharacterTextSplitter(
-        chunk_size=750, chunk_overlap=100, separators=[",", "\n", "\n\n"]
+        chunk_size=750,
+        chunk_overlap=100,
+        separators=[",", "\n", "\n\n"]
     ).split_documents(documents)
     # Create a chroma database from the documents and embeddings
     vectordb = Chroma.from_documents(
-        documents=docs, embedding=embeddings, persist_directory="db"
+        documents=docs,
+        embedding=embeddings,
+        persist_directory='db'
     )
+
+
+
+
+
+# Define a global variable for the vector database
+vectordb = None
+
+
+# Define an endpoint for uploading text files and creating embeddings
+@app.post("/upload_files/")
+async def upload_files(
+    files: list[UploadFile] = File(...),
+    embeddings: HuggingFaceEmbeddings = Depends(get_embedding_model),
+    background_tasks: BackgroundTasks = BackgroundTasks()
+):
+    # Validate that all files are text files by checking their content type
+    for file in files:
+        if not file.content_type.startswith("text/"):
+            return JSONResponse(status_code=400, content={"message": f"Invalid file type: {file.content_type}"})
+    
+    # Add a background task to process the files and create embeddings asynchronously
+    background_tasks.add_task(process_files, files, embeddings)
+
     # Return a success message with the number of documents processed
-    return {"message": f"Processed {len(documents)} documents"}
+    return {"message": f"Processed {len(files)} documents"}
 
 
 # Define an endpoint for performing similarity search on a query
-@app.get("/similarity-search/")
-async def similarity_search(query: str):
-    global vectordb  # use the global variable
+@app.get("/similarity_search/")
+async def similarity_search(
+    query: str,
+    embeddings: HuggingFaceEmbeddings = Depends(get_embedding_model)
+):
+    global vectordb # use the global variable
     # Check if the vector database is initialized
     if vectordb is None:
-        return JSONResponse(
-            status_code=400, content={"message": "No documents uploaded yet"}
-        )
+        return JSONResponse(status_code=400, content={"message": "No documents uploaded yet"})
+    # Check if the query is not empty
+    if not query:
+        return JSONResponse(status_code=400, content={"message": "Query cannot be empty"})
     # Perform similarity search on the query using the vector database
     docs = vectordb.similarity_search(query)
     # Return a list of matching documents with their metadata and page content
-    return {
-        "results": [
-            {"metadata": doc.metadata, "page_content": doc.page_content} for doc in docs
-        ]
-    }
+    return {"results": [{"metadata": doc.metadata,
+                        "page_content": doc.page_content} for doc in docs]}
+
 
 
 # Define an endpoint for asking questions over the data using RetrievalQA
 @app.get("/ask-with-data/")
-async def ask_question_with_data(question: str):
+async def ask_question_with_data(
+    question: str,
+    llm: HuggingFaceHub = Depends(get_llm),
+    prompt: PromptTemplate = Depends(get_prompt_template)
+    ):
     global vectordb  # use the global variable
-    # Check if the vector database is initialized
+# Check if the vector database is initialized
     if vectordb is None:
-        return JSONResponse(
-            status_code=400, content={"message": "No documents uploaded yet"}
-        )
+        return JSONResponse(status_code=400, content={"message": "No documents uploaded yet"})
+    # Check if the question is not empty
+    if not question:
+        return JSONResponse(status_code=400, content={"message": "Question cannot be empty"})
     # Create a RetrievalQA object using the llm, vector database and prompt template
     qa = RetrievalQA.from_chain_type(
         llm=llm,
@@ -98,13 +142,17 @@ async def ask_question_with_data(question: str):
     results = qa(question)
     # Return the answer and the source documents used to generate it
     return {
-        "answer": results["result"],
-        "source_documents": results["source_documents"],
+        "answer": results['result'],
+        "source_documents": results['source_documents']
     }
+
 
 # Define an endpoint for asking questions over the data using RetrievalQA
 @app.get("/ask/")
-async def ask_question(question: str):
+async def ask_question(
+    question: str,
+    llm: HuggingFaceHub = Depends(get_llm),
+    ):
 
     template = """
 You are a helpfull assistant. you gives helpful, summarized, and polite answers to the user's questions.
--- changelog.md
+++ changelog.md
@@ -2,3 +2,22 @@
 - using `-` instead of `underscore` for the endpoints
 - add `ask/` endpoint for asking the llms directtly without data chatgpt style
 - adding changeLog to track changes 
+
+## version 0.0.3
+- refactoring the code and big improvment across the codebase.  
+- Use dependency injection to create a single instance of the embedding model,
+the llm and the prompt template, and inject them into the endpoints that need them.
+This would avoid creating them multiple times and make the code more modular and testable.  
+
+- Use FastAPI's background tasks to process the uploaded files and create the embeddings 
+asynchronously.This would improve the performance and responsiveness of the server, 
+as it would not block the request while doing the heavy computation.  
+
+- Add some error handling and validation to the endpoints,
+such as checking if the files are valid text files, if the query or question is not empty
+if the vector database is not corrupted, etc. This would make the server more robust and user-friendly.  
+
+- Add some documentation and comments to the code, explaining what each endpoint does, what
+parameters it expects and returns, what libraries and models it uses, etc. This would make
+the code more readable and maintainable
+- add requirements.txt file
